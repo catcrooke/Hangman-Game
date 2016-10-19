4ebf125e10e7894f6d0f7c723763af098cef223d
Catherine Crooke <Cat@Catherines-MacBook-Pro.local>
2016-10-19 06:56:37 -0500

    updated pseudocode rules in .js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -35,8 +35,6 @@ function myFunction() {
     // document.getElementById("activate").style.backgroundColor = "red";
 }
 
-
-
 // create a way for the user to guess a letter from var letters 
 function chooseLetterfirstWord (){
 	if letter = 'a', then 
@@ -45,7 +43,13 @@ userGuess === 'a'
 
 // which can then be 
 
-// div id Answers that can store the blanks
+// div id word has a function that concatenates var blanks and then displays
+// them in a span
+
+
+
+// and then replaces the blanks with var words based on 
+// right/wrong letters chosen
 
 
 //blanks are replaced by letters as letters are chosen correctly
@@ -56,7 +60,6 @@ userGuess === 'a'
 // letters are stored once in the guesses div-
 
 
-
 // some sort of conditional statement?)
 
 
@@ -65,9 +68,9 @@ function chooseWord () {
     return words[Math.floor(Math.random() * words.length)];
 }
 
- // // Create code that "grabs" the div with the matching id (#drinkOptions);
+ // // Create code that "grabs" the div with the matching id (#word);
 
- // var drinkList = document.getelementbyId ('drinkOptions') 
+ // var drinkList = document.getelementbyId ('word') 
 
 //create a function that substitutes var letters indices with 
 // individual spaces in var blanks
@@ -87,4 +90,11 @@ function guessedLetters (){
 }
 return result
 
-// 
+// create a function that links the varblanks string
+ // displays the image from that city
+
+// if player answers correctly, displays image with ear intact
+// with message, "You Win! Van Gogh Keeps His Ear!"
+// else, displays image with lost ear 
+// with message, "You lost this round. Van Gogh cut off his ear. 
+// Try again with the next city where he lived!"
